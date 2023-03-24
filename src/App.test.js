cbf1bbc8a0ab3ddc838b305c0326f51c48ec57bd
Millamarianna <milla.tuomainen@outlook.com>
2023-03-24 08:16:32 +0200

    Clear todos testi

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App';
+import { render, screen, fireEvent } from'@testing-library/react';
+import'@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
+test('add & clear todo',() => {  
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Add todo
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');   
+  fireEvent.change(date, { target: { value: '29.01.2021' } })
+  fireEvent.click(screen.getByText('Add'));
+  expect(screen.getByText(/go to coffee/i)).toBeInTheDocument();
+  // Clear todo
+  fireEvent.click(screen.getByText('Clear'));
+  expect(screen.queryByText(/go to coffee/i)).not.toBeInTheDocument();
 });
